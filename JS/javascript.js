fc2cc955a7f89687f13bea09580b1702b8c5df76
JustProgrammer1 <gabrielandradepeixer@gmail.com>
2026-05-08 10:12:37 -0300

    primeira aula JS

---
+++ JS/javascript.js
@@ -0,0 +1,2 @@
+alert("Alerta em arquivo externo!");
+document.querySelector('button').innerHTML="Amanha é sábado vou estudar cedinho";
---
+++ JS/variaveis.js
@@ -0,0 +1,19 @@
+let a = 10;
+console.log(a);
+console.log(typeof a);
+a="dez";
+console.log(a);
+console.log(typeof a);
+a=true;
+console.log(a);
+console.log(typeof a);
+a=5
+console.log(a+"0")
+console.log("0" + a)
+const pi = 3.141516; //oq é o PI
+let contador =0
+contador++
+
+for(contador=0;contador<10;contador++){
+    console.log(contador);
+}
